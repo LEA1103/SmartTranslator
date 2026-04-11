37426f55333a2156eb0224ee8d01b6ec968f3483
LEA1103 <moqian108@qq.com>
2026-04-11 22:45:25 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QMainWindow w;
-    w.setWindowTitle("Translator");
-    QLabel *label = new QLabel("Build Success!", &w);
+    w.setWindowTitle("Smart Translator");
+    QLabel *label = new QLabel("CI/CD Build Success!", &w);
     label->setAlignment(Qt::AlignCenter);
     w.setCentralWidget(label);
     w.show();
